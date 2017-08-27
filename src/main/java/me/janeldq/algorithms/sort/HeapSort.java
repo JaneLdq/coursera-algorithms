a5edaba98dcea78cf3ed46862b8c07cc20b5599b
janeldq <janeldq@163.com>
2017-08-27 22:19:51 +0800

    simple min heap and heap sort

--- src/main/java/me/janeldq/algorithms/sort/HeapSort.java
+++ src/main/java/me/janeldq/algorithms/sort/HeapSort.java
@@ -1,7 +1,5 @@
 package me.janeldq.algorithms.sort;
 
-import me.janeldq.algorithms.datastructures.heap.MinHeap;
-
 /**
  * Heap Sort
  * 
@@ -11,10 +9,31 @@ import me.janeldq.algorithms.datastructures.heap.MinHeap;
 public class HeapSort implements Sort {
 
 	public <T extends Comparable<? super T>> void sort(T[] arr) {
-		MinHeap<T> heap = new MinHeap<T>(arr);
-		for(int i = 0; i < arr.length; i++) {
-			arr[i] = heap.deleteMin();
+
+		for (int i = arr.length / 2; i >= 0; i--) {
+			percolateDown(arr, i, arr.length);
 		}
+		for (int i = arr.length - 1; i > 0; i--) {
+			Util.swap(arr, 0, i);
+			percolateDown(arr, 0, i);
+		}
+	}
+	
+	private <T extends Comparable<? super T>> void percolateDown(T[] data, int index, int n) {
+		int child;
+		T tmp = data[index];
+		for(; index * 2 + 1 < n; index = child) {
+			child = index * 2 + 1;
+			if (child < n - 1 && data[child + 1].compareTo(data[child]) > 0) {
+				child++;
+			}
+			if (data[child].compareTo(tmp) > 0) {
+				data[index] = data[child];
+			} else {
+				break;
+			}
+		}
+		data[index] = tmp;
 	}
 
 	public String name() {
